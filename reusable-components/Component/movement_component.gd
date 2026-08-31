c15fc0d413ea74bc61bf1f2a32b89b2a92ca3da8
Tushar Shaw <tusharshaw28@gmail.com>
2026-08-31 16:51:11 +0530

    .

--- reusable-components/Component/movement_component.gd
+++ reusable-components/Component/movement_component.gd
@@ -1,6 +1,7 @@
 extends Node
 class_name movement_component
 @onready var parent:CharacterBody2D=get_parent()
+@onready var sprite:CharacterBody2D=get_parent().get_node("sprite2d")
 @export var speed:float=100
 @export var is_jumping=false
 @export var jump_power=-300
@@ -10,11 +11,11 @@ class_name movement_component
 @export var use_gravity := true
 @export var wall_jump_speed := 70.0
 @export var wall_jump_lock_time := 0.15
-
+var tween :Tween
 var wall_jump_timer := 0.0
 var coyote_timer:float=0
 var direction:Vector2
-
+var was_on_floor=false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -34,6 +35,14 @@ func _physics_process(delta: float) -> void:
 		parent.velocity.x=direction[0]*speed
 	jump()
 	parent.move_and_slide()
+	if not was_on_floor and parent.is_on_floor():
+		land_animation()
+
+func land_animation():
+	print("land")
+	tween=Tween.new()
+	#tween.tween_property(parent)
+	pass
 
 func jump() -> void:
 	if not can_Jump or not is_jumping:
@@ -42,12 +51,12 @@ func jump() -> void:
 	if coyote_timer > 0:
 		parent.velocity.y = jump_power
 		coyote_timer = 0
+		was_on_floor=true
 
 	elif can_wall_jump and parent.is_on_wall():
 		wall_jump_timer = wall_jump_lock_time
 		parent.velocity.y = jump_power
 		parent.velocity.x = parent.get_wall_normal().x * wall_jump_speed
-		
 		
 
 func handle_inputs(inputs: Array) -> void:
